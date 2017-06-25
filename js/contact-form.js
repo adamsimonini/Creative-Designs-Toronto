90d4a090c1e52b0fdc50e4dd11b980c062ef7f7d
Adam Simonini <adam.simonini@gmail.com>
2017-06-25 14:19:34 -0400

    Renamed Script Files

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,32 +1,35 @@
+$(document).ready(function(){
+  // Cancel default submit function and replace with custom JS
+    $("#submitButton").click(function(e){
+        // cancels the form submission
+        e.preventDefault();
+        submitForm();
+    });
 
-// Cancel default submit function and replace with custom JS
-  $("#contactForm").submit(function(event){
-      // cancels the form submission
-      event.preventDefault();
-      submitForm();
+  // Contact Form
+    function submitForm(){
+        // Initiate Variables With Form Content
+        var name = $("#clientName").val();
+        var email = $("#clientEmail").val();
+        var inquiry =$("#inquiry").val();
+        var message = $("#clientMessage").val();
+        console.log("start");
+
+        $.ajax({
+            type: "POST",
+            url: "https://www.creativedesignstoronto.com/php/contact-form.php",
+            dataType: 'jsonp',
+            data: "name=" + name + "&email=" + email + "&message=" + message,
+            success: function(text){
+                if (text == "success"){
+                    console.log("success!");
+                    formSuccess();
+                }
+            }
+        });
+    }
+    function formSuccess(){
+        console.log("formSuccess success!");
+        $("#msgSubmit").removeClass("invisible");
+    }
   });
-
-// Contact Form
-  function submitForm(){
-      // Initiate Variables With Form Content
-      var name = $("#clientName").val();
-      var email = $("#clientEmail").val();
-      var inquiry =$("#inquiry").val();
-      var message = $("#clientMessage").val();
-
-      $.ajax({
-          type: "POST",
-          url: "php/contact-form.php",
-          data: "name=" + name + "&email=" + email + "&message=" + message,
-          success: function(text){
-              if (text == "success"){
-                  console.log("success!");
-                  formSuccess();
-              }
-          }
-      });
-  }
-  function formSuccess(){
-      console.log("formSuccess success!");
-      $("#msgSubmit").removeClass("hidden");
-  }
